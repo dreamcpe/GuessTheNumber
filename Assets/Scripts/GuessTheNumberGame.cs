96a1d425dac772e74d191f80d7369284c2127a5f
Kampee Intapong <dreamcpe@icloud.com>
2018-10-25 05:01:56 +0700

    finish guess the number game

---
+++ Assets/Scripts/GuessTheNumberGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuessTheNumberGame : MonoBehaviour {
+
+
+    public InputField input;
+    public Text infoText;
+
+    private int guessNumber;
+    private int userGuess;
+
+	// Use this for initialization
+	void Start () {
+
+        // สุ่มตัวเลขขึ้นมา 1 ตัว ระหว่าง 0-100
+        guessNumber = Random.Range(0, 100);
+		
+	}
+
+    public void CheckGuess(){
+
+        // รับค่าจากผู้เล่นในช่องทายตัวเลข แปลงค่า text เป็น int
+        userGuess = int.Parse(input.text);
+
+        // ใช้ if else เช็คตัวเลข
+        if(userGuess == guessNumber){
+            infoText.text = "ถูกต้องนะคร๊าบบบบบ" ;
+        } else if (userGuess > guessNumber) {
+            infoText.text = "ตัวเลขยังมากกว่า" ;
+        } else if (userGuess < guessNumber) {
+            infoText.text = "ตัวเลขยังน้อยกว่า";
+        }
+
+        input.text = "";
+    }
+	
+}
